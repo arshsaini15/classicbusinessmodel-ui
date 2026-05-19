40e99cbe0d6315134d23092de39d0c96f2a6b07e
rachit069 <rachit694.be22@chitkara.edu.in>
2026-05-19 11:43:54 +0530

    initial setup

---
+++ src/main/java/com/project/cmb/CmbApplication.java
@@ -0,0 +1,13 @@
+package com.project.cmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cmb/CmbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cmb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
